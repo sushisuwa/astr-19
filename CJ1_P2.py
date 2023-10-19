67afdea566222766fac780aca942ea94e5f2e66c
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2023-10-19 04:58:39 -0700

    Adding Code Journal 1 Prompt 2

---
+++ CJ1_P2.py
@@ -0,0 +1,32 @@
+#Define 2 floating points
+a = 1.0
+b = 2.0
+
+#Add them
+c = a + b
+
+#print the result and its data type
+print(c)
+print(type(c))
+
+#Define 2 integers
+x = 3
+y = 1
+
+#Find the difference between them
+z = x - y 
+
+#print the result and its data type
+print(z)
+print(type(z))
+
+#Define a floating point number and an integer
+p = 3.0
+o = 4
+
+#Find the product
+g = p * o 
+
+#Print the result and its data type
+print(g)
+print(type(g))
