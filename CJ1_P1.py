90fb75a1a8ccf114d167475646c0a1370629438a
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2023-10-19 04:44:23 -0700

    Adding Code Journal 1 Prompt 1

---
+++ CJ1_P1.py
@@ -0,0 +1,10 @@
+def MyInfo():
+	print("Jalen Suwa\nHe/Him")
+	print("My favorite movie is Everything Everywhere All at Once.")
+	print("My favorite food is a nice chicken parm.")
+
+def main():
+	MyInfo()
+
+if __name__ == '__main__':
+	main()
